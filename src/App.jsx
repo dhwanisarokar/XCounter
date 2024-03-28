a8b771f2fffdbe0282891b99670b44df11608fda
dhwanisarokar <dhwanisarokar@gmail.com>
2024-03-28 16:03:11 +0530

    impliment counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  const handleIncrement = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+  const handleDecrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  return (
+    <>
+      <h1>Counter App</h1>
+      <div>
+        <p>Count: {count}</p>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleDecrement}>Decrement</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
